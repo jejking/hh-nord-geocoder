bf1684de9c975c2cf605a65ffc4e09c8192a3158
jejking <jejking@yahoo.de>
2014-05-04 19:05:46 +0200

    import of streets to neo4j done

---
+++ src/main/java/info/jejking/hamburg/nord/geocoder/hh/OsmStreetCollectionToNeoImporter.java
@@ -0,0 +1,88 @@
+/* 
+ *  Hamburg-Nord Geocoder, by John King.
+ *  Copyright (C) 2014,  John King
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+package info.jejking.hamburg.nord.geocoder.hh;
+
+import static info.jejking.hamburg.nord.geocoder.hh.GazetteerNames.ADMINISTRATIVE;
+import static info.jejking.hamburg.nord.geocoder.hh.GazetteerNames.NAME;
+import static info.jejking.hamburg.nord.geocoder.hh.GazetteerEntryTypes.STREET;
+import static info.jejking.hamburg.nord.geocoder.hh.GazetteerNames.GAZETTEER_FULLTEXT;
+import static info.jejking.hamburg.nord.geocoder.hh.GazetteerNames.TYPE;
+
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.geotools.referencing.crs.DefaultGeographicCRS;
+import org.neo4j.gis.spatial.EditableLayer;
+import org.neo4j.gis.spatial.SpatialDatabaseRecord;
+import org.neo4j.gis.spatial.SpatialDatabaseService;
+import org.neo4j.graphdb.DynamicLabel;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.index.Index;
+
+import com.vividsolutions.jts.geom.Geometry;
+
+/**
+ * Imports a collection of name-to-geometry mappings generated
+ * from {@link OsmStreetCollectionBuilder} into Neo4j.
+ * 
+ * 
+ * 
+ * @author jejking
+ *
+ */
+public class OsmStreetCollectionToNeoImporter {
+
+    public void writeNoNeo(Map<String, Geometry> streets, GraphDatabaseService graph) {
+        SpatialDatabaseService spatialDatabaseService = new SpatialDatabaseService(graph);
+        
+        try (Transaction tx = graph.beginTx()) {
+            EditableLayer adminLayer = getEditableLayer(spatialDatabaseService, ADMINISTRATIVE);
+            EditableLayer streetLayer = getEditableLayer(spatialDatabaseService, STREET);
+            Index<Node> fullText = graph.index().forNodes(GAZETTEER_FULLTEXT);
+            
+            for (Entry<String, Geometry> entry : streets.entrySet()) {
+                addStreet(entry.getKey(), entry.getValue(), streetLayer, adminLayer, fullText);
+            }
+            
+            tx.success();
+        }
+        
+    }
+
+    private void addStreet(String name, Geometry geometry, EditableLayer streetLayer, EditableLayer adminLayer, Index<Node> fullText) {
+        SpatialDatabaseRecord record = streetLayer.add(geometry, new String[]{NAME}, new Object[]{name});
+        Node neoNode = record.getGeomNode();
+        neoNode.addLabel(DynamicLabel.label(STREET));
+                
+        fullText.add(neoNode, NAME, name);
+        fullText.add(neoNode, TYPE, STREET);
+        
+        // next, try and put the street record in relationship to a containing admin polygon
+        
+    }
+
+    private EditableLayer getEditableLayer(SpatialDatabaseService spatialDatabaseService, String name) {
+        EditableLayer layer = (EditableLayer) spatialDatabaseService.createWKBLayer(name);
+        layer.setCoordinateReferenceSystem(DefaultGeographicCRS.WGS84);
+        return layer;
+    }
+    
+}
---
+++ src/test/java/info/jejking/hamburg/nord/geocoder/hh/OsmStreetCollectionToNeoImporterTest.java
@@ -0,0 +1,205 @@
+/* 
+ *  Hamburg-Nord Geocoder, by John King.
+ *  Copyright (C) 2014,  John King
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+package info.jejking.hamburg.nord.geocoder.hh;
+
+import static info.jejking.hamburg.nord.geocoder.hh.GazetteerEntryTypes.STREET;
+import static info.jejking.hamburg.nord.geocoder.hh.GazetteerNames.GAZETTEER_FULLTEXT;
+import static info.jejking.hamburg.nord.geocoder.hh.GazetteerNames.NAME;
+import static info.jejking.hamburg.nord.geocoder.hh.GazetteerNames.TYPE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import info.jejking.hamburg.nord.geocoder.GeographicFunctions;
+
+import java.util.List;
+import java.util.Map;
+
+import org.jaitools.jts.CoordinateSequence2D;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.neo4j.gis.spatial.Layer;
+import org.neo4j.gis.spatial.SpatialDatabaseService;
+import org.neo4j.gis.spatial.pipes.GeoPipeline;
+import org.neo4j.graphdb.DynamicLabel;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.ResourceIterator;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.index.Index;
+import org.neo4j.graphdb.index.IndexManager;
+import org.neo4j.graphdb.schema.Schema;
+import org.neo4j.helpers.collection.MapUtil;
+import org.neo4j.test.TestGraphDatabaseFactory;
+
+import com.vividsolutions.jts.geom.Envelope;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.Point;
+
+
+public class OsmStreetCollectionToNeoImporterTest {
+
+    private static GraphDatabaseService graph;
+    private static SpatialDatabaseService spatialDatabaseService;
+    
+    private static Map<String, Geometry> streets;
+    
+    @BeforeClass
+    public static void init() {
+        
+        graph = new TestGraphDatabaseFactory()
+                    .newImpermanentDatabaseBuilder()
+                    .newGraphDatabase();
+        
+        buildIndexes();
+        spatialDatabaseService = new SpatialDatabaseService(graph);
+        
+        OsmStreetCollectionBuilder builder = new OsmStreetCollectionBuilder();
+        streets = builder.buildRawStreetCollection();
+        
+        OsmStreetCollectionToNeoImporter importer = new OsmStreetCollectionToNeoImporter();
+        importer.writeNoNeo(streets, graph);
+        
+    }
+    
+    @Test
+    public void writtenToNeo() {
+        // retrieve Itzehoer Weg and Mundsburger Damm
+        
+        try (Transaction tx = graph.beginTx()) {
+            
+            try (ResourceIterator<Node> iterator = graph
+                                                    .findNodesByLabelAndProperty(
+                                                            DynamicLabel.label(GazetteerEntryTypes.STREET),
+                                                            NAME, "Itzehoer Weg")
+                                                    .iterator()) {
+                Node hh = iterator.next();
+                
+                assertTrue(hh.hasLabel(DynamicLabel.label(STREET)));
+                
+                assertFalse(iterator.hasNext());
+            }
+            
+            try (ResourceIterator<Node> iterator = graph
+                    .findNodesByLabelAndProperty(
+                            DynamicLabel.label(GazetteerEntryTypes.STREET),
+                            NAME, "Mundsburger Damm")
+                    .iterator()) {
+                Node hh = iterator.next();
+                
+                assertTrue(hh.hasLabel(DynamicLabel.label(STREET)));
+                assertFalse(iterator.hasNext());
+            }
+            
+        }
+        
+    }
+    
+    @Test
+    public void fullTextQuery() {
+        try (Transaction tx = graph.beginTx()) {
+            // test with get...
+            Index<Node> fullText = graph.index().forNodes(GAZETTEER_FULLTEXT);
+            
+            int mundsCount = 0;
+            boolean foundBruecke = false;
+            boolean foundDamm = false;
+            for (Node mundsburg : fullText.query(NAME + ":Mundsburg* AND " + TYPE + ":" + STREET)) {
+                mundsCount++;  
+                if (mundsburg.getProperty(NAME).equals("Mundsburger Brücke")) {
+                    foundBruecke = true;
+                }
+                if (mundsburg.getProperty(NAME).equals("Mundsburger Damm")) {
+                    foundDamm = true;
+                }
+            }
+            
+            assertEquals(2, mundsCount);
+            assertTrue(foundBruecke);
+            assertTrue(foundDamm);
+        }
+    }
+    
+    @Test
+    public void spatialQuery() {
+        Layer streets = spatialDatabaseService.getLayer(STREET);
+        // bus stop Mundsburger Brücke stadtauswärts (37, 172)
+        Point point = new Point(new CoordinateSequence2D(10.0206119, 53.5660032), streets.getGeometryFactory());
+        
+        Envelope env = GeographicFunctions.computeEnvelopeAroundPoint(point, 50);
+        
+        int streetCount = 0;
+        boolean foundHartwicus = false;
+        boolean foundMundsburgerBruecke = false;
+        boolean foundMundsburgerDamm = false;
+        boolean foundArmgartstraße = false;
+        
+        try (Transaction tx = graph.beginTx()) {
+            List<Node> nodes = GeoPipeline
+                                    .startIntersectWindowSearch(streets, env)
+                                    .toNodeList();
+            for (Node streetNode : nodes) {
+                String streetName = (String) streetNode.getProperty(NAME);
+                streetCount++;
+                switch (streetName) {
+                    case "Hartwicusstraße" : foundHartwicus = true; break;
+                    case "Mundsburger Brücke" : foundMundsburgerBruecke = true; break;
+                    case "Mundsburger Damm" : foundMundsburgerDamm = true; break;
+                    case "Armgartstraße" : foundArmgartstraße = true; break;
+                    default : continue;
+                }
+            }
+            assertEquals(4, streetCount);
+            assertTrue(foundHartwicus);
+            assertTrue(foundMundsburgerBruecke);
+            assertTrue(foundMundsburgerDamm);
+            assertTrue(foundArmgartstraße);
+        }
+        
+    }
+    
+    private static void buildIndexes() {
+        
+        // we want an additional index on adminstrative area - name
+        try (Transaction tx = graph.beginTx()) {
+            Schema schema = graph.schema();
+            schema
+                .indexFor(DynamicLabel.label(STREET))
+                .on("NAME")
+                .create();
+            
+            
+            IndexManager indexManager = graph.index();
+            @SuppressWarnings("unused")
+            Index<Node> fullText = indexManager.forNodes(GAZETTEER_FULLTEXT,
+                                MapUtil.stringMap(IndexManager.PROVIDER, "lucene",
+                                                  "type", "fulltext"));
+            
+            tx.success();
+        }
+        
+        
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        graph.shutdown();
+    }
+
+}

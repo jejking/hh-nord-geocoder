2c9d33e6c664636982b73f9d7c460497a3132dc3
jejking <jejking@yahoo.de>
2014-05-11 21:09:34 +0200

    Useful for manual testing of large files.

---
+++ src/test/java/info/jejking/osm/CountingOsmParserRunner.java
@@ -0,0 +1,51 @@
+/* 
+ *  Hamburg-Nord Geocoder, by John King.
+ *  Copyright (C) 2014,  John King
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ *    
+ */
+package info.jejking.osm;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+
+
+public class CountingOsmParserRunner {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Exception {
+        String fileName = args[0];
+        
+        EventCounter<OsmNode> nodeCounter = new EventCounter<>("node");
+        EventCounter<OsmWay> wayCounter = new EventCounter<>("way");
+        EventCounter<OsmRelation> relCounter = new EventCounter<>("relation");
+        
+        OsmParser parser = new OsmParser();
+        parser.getOsmNodeObservable().subscribe(nodeCounter);
+        parser.getOsmWayObservable().subscribe(wayCounter);
+        parser.getOsmRelationObservable().subscribe(relCounter);
+        parser.parseOsmStream(new BufferedInputStream(new FileInputStream(fileName)));
+        
+        System.out.println("done");
+        System.out.println("Nodes counted: " + nodeCounter.count);
+        System.out.println("Ways counted: " + wayCounter.count);
+        System.out.println("Relations counted: " + relCounter.count);
+        
+    }
+
+}
---
+++ src/test/java/info/jejking/osm/EventCounter.java
@@ -0,0 +1,51 @@
+/* 
+ *  Hamburg-Nord Geocoder, by John King.
+ *  Copyright (C) 2014,  John King
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ *    
+ */
+package info.jejking.osm;
+
+
+class EventCounter<T> implements SimpleObserver<T> {
+
+    long count = 0;
+    boolean completed;
+    String type;
+    
+    
+    public EventCounter(String type) {
+        this.type = type;
+    }
+    
+    
+    @Override
+    public void onNext(T value) {
+        count++;
+    }
+
+    @Override
+    public void onError(Exception e) {
+        e.printStackTrace();
+    }
+
+    @Override
+    public void onCompleted() {
+        System.out.println("Completed " + type);
+        this.completed = true;
+    }
+    
+}
